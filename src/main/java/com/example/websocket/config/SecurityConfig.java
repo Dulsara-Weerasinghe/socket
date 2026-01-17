a8d6ba46a66d368672258743722cc3c6ff082b27
randula.dela <randula.dela@circles.asia>
2026-01-17 21:04:46 +0530

    h2 console added

--- src/main/java/com/example/websocket/config/SecurityConfig.java
+++ src/main/java/com/example/websocket/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/ws/**").permitAll()
+                        .requestMatchers("/auth/**", "/ws/**","/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
